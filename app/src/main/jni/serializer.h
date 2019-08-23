18f145b86acee305ad9d9cd44e20c25d96477ed9
mgood7123 <smallville7123@gmail.com>
2019-08-23 13:38:42 +1000

    updated serializer

--- app/src/main/jni/serializer.h
+++ app/src/main/jni/serializer.h
@@ -10,24 +10,76 @@
 #include <stdio.h> // printf
 #include <memory.h> // mem*
 #include <inttypes.h>
+#include <cerrno> // errno, errors
+#include <stdlib.h> // malloc/realloc/free
 
 #ifndef __ANDROID__
-#define LOG_INFO printf
-    #define LOG_ERROR printf
+#define LOG_INFO_serializer printf
+    #define LOG_ERROR_serializer printf
 #else
 
-#include <strings.h>
+    #include <strings.h>
     #include <android/log.h>
 
-    #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-    #define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+    #define LOG_TAG_serializer "serializer"
+    #define LOG_INFO_serializer(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG_serializer, __VA_ARGS__)
+    #define LOG_ERROR_serializer(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG_serializer, __VA_ARGS__)
 #endif
 
 
 struct serializer_data {
-    int type_size;
-    void *data;
+    int8_t type_size;
+    int8_t *data;
     size_t data_len;
+};
+
+class serializer_stream {
+    public:
+        int8_t *data = nullptr;
+        size_t data_len = 0;
+
+        bool allocate(size_t len) {
+            if (data == nullptr) {
+                data = static_cast<int8_t *>(malloc(len));
+                if (data == NULL) if (errno == ENOMEM) return false;
+                data_len = len;
+                memset(data, 0, data_len);
+            }
+            return true;
+        }
+
+        bool resize(size_t len) {
+            if (data == nullptr) return allocate(len);
+            if (data_len == len) return true;
+            int8_t *data_tmp = static_cast<int8_t *>(realloc(data, len));
+            if (data_tmp == NULL) if (errno == ENOMEM) return false;
+            data = data_tmp;
+            data_len = len;
+            return true;
+        }
+
+        bool deallocate() {
+            if (data == nullptr) return true;
+            else free(data);
+        }
+
+        bool append(size_t len) {
+            return resize(data_len + len);
+        }
+
+        bool retract(size_t len) {
+            resize(data_len - len);
+        }
+
+        serializer_stream retract_from_front(size_t len) {
+            assert(data != NULL);
+            serializer_stream data_;
+            data_.allocate(len);
+            memcpy(data_.data, data, len);
+            memmove(data, data + len, data_len - len);
+            resize(data_len - len);
+            return data_;
+        }
 };
 
 typedef std::deque<struct serializer_data> Serial;
@@ -36,15 +88,15 @@ class serializer {
     private:
         template<typename type, typename matches>
         bool add_if_matches(type *data, size_t data_len) {
-            int ts = sizeof(type);
+            int8_t ts = sizeof(type);
             if (ts == sizeof(matches)) {
                 matches *x = new matches[data_len];
                 memcpy(x, data, sizeof(matches) * data_len);
                 struct serializer_data s;
                 s.type_size = ts;
-                s.data = x;
+                reinterpret_cast<matches **>(&s.data)[0] = x; // lvalue assign not allowed
                 s.data_len = data_len;
-                serial.push_back(s);
+                in.push_back(s);
                 return true;
             }
             return false;
@@ -54,29 +106,31 @@ class serializer {
         bool remove_if_matches(type *data) {
             int ts = sizeof(type);
             if (ts == sizeof(matches)) {
-                struct serializer_data data_ = serial.front();
-                serial.pop_front();
-                memcpy(data, static_cast<matches *>(data_.data), sizeof(matches) * data_.data_len);
+                struct serializer_data data_ = out.front();
+                out.pop_front();
+                memcpy(data, reinterpret_cast<matches *>(data_.data),
+                       sizeof(matches) * data_.data_len);
                 return true;
             }
             return false;
         }
 
         template<typename type, typename matches>
-        bool remove_pointer_if_matches(type **data) {
+        size_t remove_pointer_if_matches(type **data) {
             int ts = sizeof(type);
             if (ts == sizeof(matches)) {
-                struct serializer_data data_ = serial.front();
-                serial.pop_front();
+                struct serializer_data data_ = out.front();
+                out.pop_front();
                 *static_cast<type **>(data) = new type[data_.data_len];
-                memcpy(*data, static_cast<type *>(data_.data), sizeof(type) * data_.data_len);
-                return true;
+                memcpy(*data, reinterpret_cast<type *>(data_.data), sizeof(type) * data_.data_len);
+                return data_.data_len;
             }
-            return false;
+            return 0;
         }
-
-        Serial serial;
     public:
+        Serial in;
+        struct serializer_stream stream;
+        Serial out;
         template<typename TYPE>
         bool add(TYPE data) {
             return add_pointer<TYPE>(&data, 1);
@@ -101,31 +155,92 @@ class serializer {
         }
 
         template<typename TYPE>
-        bool get_pointer(TYPE **data) {
-            if (remove_pointer_if_matches<TYPE, int8_t>(data)) return true;
-            if (remove_pointer_if_matches<TYPE, int16_t>(data)) return true;
-            if (remove_pointer_if_matches<TYPE, int32_t>(data)) return true;
-            if (remove_pointer_if_matches<TYPE, int64_t>(data)) return true;
-            return false;
+        size_t get_pointer(TYPE **data) {
+            size_t s;
+            s = remove_pointer_if_matches<TYPE, int8_t>(data);
+            if (s != 0) return s;
+            s = remove_pointer_if_matches<TYPE, int16_t>(data);
+            if (s != 0) return s;
+            s = remove_pointer_if_matches<TYPE, int32_t>(data);
+            if (s != 0) return s;
+            s = remove_pointer_if_matches<TYPE, int64_t>(data);
+            if (s != 0) return s;
+            return 0;
+        }
+
+        void construct() {
+            size_t index = 0;
+            while (in.size() != 0) {
+                // type_size (1), data length (8), data (*)
+                struct serializer_data data_ = in.front();
+                in.pop_front();
+                stream.append(sizeof(int8_t) * 1);
+                stream.data[index++] = data_.type_size;
+                if (data_.type_size == sizeof(int8_t)) {
+                    stream.append(sizeof(int8_t) * sizeof(int8_t));
+                    reinterpret_cast<int8_t *>(stream.data +
+                                               index)[0] = static_cast<int8_t>(data_.data_len);
+                    index += sizeof(int8_t);
+                } else if (data_.type_size == sizeof(int16_t)) {
+                    stream.append(sizeof(int8_t) * sizeof(int16_t));
+                    reinterpret_cast<int16_t *>(stream.data +
+                                                index)[0] = static_cast<int16_t>(data_.data_len);
+                    index += sizeof(int16_t);
+                } else if (data_.type_size == sizeof(int32_t)) {
+                    stream.append(sizeof(int8_t) * sizeof(int32_t));
+                    reinterpret_cast<int32_t *>(stream.data + index)[0] = data_.data_len;
+                    index += sizeof(int32_t);
+                } else if (data_.type_size == sizeof(int64_t)) {
+                    stream.append(sizeof(int8_t) * sizeof(int64_t));
+                    reinterpret_cast<int64_t *>(stream.data + index)[0] = data_.data_len;
+                    index += sizeof(int64_t);
+                }
+                stream.append(data_.type_size * data_.data_len);
+                memcpy(&stream.data[index], data_.data, data_.type_size * data_.data_len);
+                index += data_.type_size * data_.data_len;
+            }
+        }
+
+        void deconstruct() {
+            while (stream.data_len != 0) {
+                struct serializer_data data2;
+                // type_size (1), data length (8), data (*)
+                serializer_stream type = stream.retract_from_front(sizeof(int8_t) * 1);
+                memcpy(&data2.type_size, type.data, type.data_len);
+                serializer_stream len = stream.retract_from_front(
+                    static_cast<size_t>(data2.type_size));
+                memcpy(&data2.data_len, len.data, len.data_len);
+                serializer_stream data = stream.retract_from_front(type.data[0] * data2.data_len);
+                data2.data = static_cast<int8_t *>(malloc(type.data[0] * data.data_len));
+                memcpy(data2.data, data.data, type.data[0] * data.data_len);
+                out.push_back(data2);
+            }
         }
 };
 
 void ser() {
-    serializer X;
-    X.add<size_t>(5);
-    X.add<size_t>(3);
-    size_t size[2];
-    X.get<size_t>(&size[0]);
-    X.get<size_t>(&size[1]);
-    LOG_INFO("X[0] is %zu, X[1] is %zu\n", size[0], size[1]);
-    assert(size[0] == 5);
-    assert(size[1] == 3);
-    X.add_pointer<size_t>(size, 2);
-    size_t *size_;
-    X.get_pointer<size_t>(&size_);
-    LOG_INFO("X[0] is %zu, X[1] is %zu\n", size_[0], size_[1]);
-    assert(size_[0] == 5);
-    assert(size_[1] == 3);
+    serializer XX;
+    XX.add<size_t>(5);
+    XX.add<int>(3);
+    XX.add<double>(6.8);
+    size_t size2[2] = {55, 55};
+    XX.add_pointer<size_t>(size2, 2);
+    XX.construct();
+    XX.deconstruct();
+    size_t V1;
+    XX.get<size_t>(&V1);
+    LOG_INFO_serializer("V1 = %zu\n", V1);
+    int V2;
+    XX.get<int>(&V2);
+    LOG_INFO_serializer("V2 = %d\n", V2);
+    double V3;
+    XX.get<double>(&V3);
+    LOG_INFO_serializer("V3 = %G\n", V3);
+    size_t *V4;
+    size_t indexes = XX.get_pointer<size_t>(&V4);
+    LOG_INFO_serializer("indexes = %zu\n", indexes);
+    LOG_INFO_serializer("V4[0] = %zu\n", V4[0]);
+    LOG_INFO_serializer("V4[1] = %zu\n", V4[1]);
 }
 
 #endif //GLNE_SERIALIZER_H
