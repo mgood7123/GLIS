154bed2966e667f4510dfd7a800272ef3ed06979
mgood7123 <smallville7123@gmail.com>
2020-07-10 12:43:06 +1000

    add doc for unlink

--- app/src/main/jni/SysV_api/SysV.cpp
+++ app/src/main/jni/SysV_api/SysV.cpp
@@ -25,6 +25,16 @@ int shm_open(const char *name, int oflag, mode_t mode) {
     return fd;
 }
 
+/*
+If one or more references to the shared memory object exist when the object is unlinked, the name shall be removed before shm_unlink() returns, but the removal of the memory object contents shall be postponed until all open and map references to the shared memory object have been removed.
+
+Even if the object continues to exist after the last shm_unlink(), reuse of the name shall subsequently cause shm_open() to behave as if no shared memory object of this name exists (that is, shm_open() will fail if O_CREAT is not set, or will create a new shared memory object if O_CREAT is set).
+
+
+
+i could create a name_count however this only accounts for open/close, it does not account for mmap/munmap
+*/
+
 int shm_unlink(const char *name) {
     std::string path = resolve_name(name);
     unlink(path.c_str());
