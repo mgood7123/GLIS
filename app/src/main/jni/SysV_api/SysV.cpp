2bfc237b348504a0e89679d7cfe0883c201b2ce9
mgood7123 <smallville7123@gmail.com>
2020-07-10 12:59:12 +1000

    add more docs

--- app/src/main/jni/SysV_api/SysV.cpp
+++ app/src/main/jni/SysV_api/SysV.cpp
@@ -17,6 +17,19 @@ case 5: shm_open; mmap; shm_unlink;
 case 6: shm_open; mmap; shm_unlink; munmap;
 case 7: shm_open; mmap; munmap; shm_unlink;
 
+on open():
+    the file is created in /tmp/FILE_NAME
+on mmap():
+    a reference is associated with /tmp/FILE_NAME
+on munmap():
+    the reference to /tmp/FILE_NAME is removed
+    if the file /tmp/FILE_NAME is queued for removal then
+        the file /tmp/FILE_NAME is removed after the reference is removed
+on unlink():
+    the file /tmp/FILE_NAME is removed
+    if the file /tmp/FILE_NAME is associated to a reference
+        then the file /tmp/FILE_NAME is queued for removal
+        upon the reference to /tmp/FILE_NAME being removed
 */
 
 char * shm_root_directory = const_cast<char*>("/tmp");
@@ -42,10 +55,6 @@ int shm_open(const char *name, int oflag, mode_t mode) {
 If one or more references to the shared memory object exist when the object is unlinked, the name shall be removed before shm_unlink() returns, but the removal of the memory object contents shall be postponed until all open and map references to the shared memory object have been removed.
 
 Even if the object continues to exist after the last shm_unlink(), reuse of the name shall subsequently cause shm_open() to behave as if no shared memory object of this name exists (that is, shm_open() will fail if O_CREAT is not set, or will create a new shared memory object if O_CREAT is set).
-
-
-
-i could create a name_count however this only accounts for open/close, it does not account for mmap/munmap
 */
 
 int shm_unlink(const char *name) {
