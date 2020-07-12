0a8175b61b00ad37c0be7e81cfe251e41be30695
mgood7123 <smallville7123@gmail.com>
2020-07-12 17:55:34 +1000

    split fps.hpp into fps.hpp and fps.cpp

--- app/src/main/jni/GLIS/include/glis/internal/fps.hpp
+++ app/src/main/jni/GLIS/include/glis/internal/fps.hpp
@@ -8,31 +8,12 @@ class GLIS_FPS {
     int queueAggregate = 0;
     std::queue<int> frameLengths;
 
-    long getCurrentTime(void) {
-        struct timeval tv = {0};
-        gettimeofday(&tv, NULL);
-        return tv.tv_sec * 1000. + tv.tv_usec / 1000.;
-    }
+    long getCurrentTime(void);
 
 public:
     double averageFps;
 
-    void onFrameStart() {
-        frameStartTime = getCurrentTime();
-    }
+    void onFrameStart();
 
-    void onFrameEnd() {
-        int frameLength = getCurrentTime() - frameStartTime;
-
-        frameLengths.push(frameLength);
-        queueAggregate += frameLength;
-
-        while (queueAggregate > trackedTime) {
-            int oldFrame = frameLengths.front();
-            frameLengths.pop();
-            queueAggregate -= oldFrame;
-        }
-
-        averageFps = frameLengths.size() / (trackedTime / 1000);
-    }
+    void onFrameEnd();
 };
---
+++ app/src/main/jni/GLIS/src/glis/internal/fps.cpp
@@ -0,0 +1,25 @@
+
+long GLIS_FPS::getCurrentTime(void) {
+    struct timeval tv = {0};
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000. + tv.tv_usec / 1000.;
+}
+
+void GLIS_FPS::onFrameStart() {
+    frameStartTime = getCurrentTime();
+}
+
+void GLIS_FPS::onFrameEnd() {
+    int frameLength = getCurrentTime() - frameStartTime;
+
+    frameLengths.push(frameLength);
+    queueAggregate += frameLength;
+
+    while (queueAggregate > trackedTime) {
+        int oldFrame = frameLengths.front();
+        frameLengths.pop();
+        queueAggregate -= oldFrame;
+    }
+
+    averageFps = frameLengths.size() / (trackedTime / 1000);
+}
