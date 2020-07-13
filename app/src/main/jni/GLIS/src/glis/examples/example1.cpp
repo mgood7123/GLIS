24b5635d505d147f625ed26086f101155c29c38b
mgood7123 <smallville7123@gmail.com>
2020-07-14 00:01:17 +1000

    add X11 toggle to example

--- app/src/main/jni/GLIS/src/glis/examples/example1.cpp
+++ app/src/main/jni/GLIS/src/glis/examples/example1.cpp
@@ -2,16 +2,19 @@
 
 #include <glis/glis.hpp>
 
+bool useX11 = false;
+
 int main() {
     // print non errors
     GLIS_LOG_PRINT_NON_ERRORS = true;
     GLIS glis;
     GLIS_CLASS glis_class;
 
-//    // create a new X11 window
-//    if (!glis.getX11Window(glis_class, 400, 400)) return -1;
-    // create a new Wayland window
-    if (!glis.getWaylandWindow(glis_class, 400, 400)) return -1;
+    if (useX11) {
+        if (!glis.getX11Window(glis_class, 400, 400)) return -1;
+    } else {
+        if (!glis.getWaylandWindow(glis_class, 400, 400)) return -1;
+    }
 
     // initiate opengl
     glis.GLIS_setupOnScreenRendering(glis_class);
@@ -27,14 +30,19 @@ int main() {
     glis.GLIS_SwapBuffers(glis_class);
     glis.GLIS_error_to_string_EGL("SwapBuffers");
 
-    while (glis.waylandDispatch(glis_class) != -1 && glis.waylandIsRunning());
-
-    // sleep to give time for window to appear
-//    sleep(2);
+    if (useX11) {
+        // sleep to give time for window to appear
+        sleep(2);
+    } else {
+        while (glis.waylandDispatch(glis_class) != -1 && glis.waylandIsRunning());
+    }
 
     // clean up everything
     glis.GLIS_destroy_GLIS(glis_class);
-//    glis.destroyX11Window(glis_class);
-    glis.destroyWaylandWindow(glis_class);
+    if (useX11) {
+        glis.destroyX11Window(glis_class);
+    } else {
+        glis.destroyWaylandWindow(glis_class);
+    }
     return 0;
 }
