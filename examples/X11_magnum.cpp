87e4dfe22eab823007bcfcfcaa10bfdfb61a7087
mgood7123 <smallville7123@gmail.com>
2020-08-13 01:12:13 +1000

    add posible wrappers for magnum and gl

--- examples/X11_magnum.cpp
+++ examples/X11_magnum.cpp
@@ -6,12 +6,46 @@
 #include <Magnum/Shaders/Flat.h>
 #include <Magnum/Primitives/Plane.h>
 
+#include <Magnum/GL/Texture.h>
+
 GLIS_CLASS screen;
 GLIS glis;
 GLIS_FONT font;
 GLIS_FPS fps;
 
 using namespace Magnum;
+
+void setTextureData(GL::Texture2D & texture, int texture_width, int texture_height, void * data) {
+
+    // is this safe to be function scoped?
+    ImageView2D image(PixelFormat::RGBA8Unorm, {texture_width, texture_height}, data);
+    
+    // do mipmaps need to be regenerated if the data changes?
+    texture.setSubImage(0, {}, image).generateMipmap();
+}
+
+GL::Texture2D & genTextureFromGLTexture() {
+    GL::Texture2D texture;
+    texture.wrap(GLuint id);
+    // would anything else need to be done?
+    return texture;
+}
+
+GL::Texture2D & genTextureFromData(int texture_width, int texture_height, void * data) {
+
+    // is this safe to be function scoped?
+    ImageView2D image(PixelFormat::RGBA8Unorm, {texture_width, texture_height}, data);
+
+    GL::Texture2D texture;
+    texture.setMagnificationFilter(GL::SamplerFilter::Linear)
+        .setMinificationFilter(GL::SamplerFilter::Linear, GL::SamplerMipmap::Linear)
+        .setWrapping(GL::SamplerWrapping::ClampToEdge)
+        .setMaxAnisotropy(GL::Sampler::maxMaxAnisotropy())
+        .setStorage(Math::log2(4096)+1, GL::TextureFormat::RGBA8, {texture_width, texture_height})
+        .setSubImage(0, {}, image)
+        .generateMipmap();
+    return texture;
+}
 
 GLIS_CALLBACKS_DRAW_RESIZE_CLOSE(draw, glis, renderer, font, fps) {
     GL::defaultFramebuffer.clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
