051287cf9e0c26f324893eaa1ab13cb8e7aff06e
mgood7123 <smallville7123@gmail.com>
2020-08-24 13:47:30 +1000

    add compile-time sorting :)

--- examples/compile_time_optimization_test.cpp
+++ examples/compile_time_optimization_test.cpp
@@ -13,9 +13,10 @@ struct array {
     constexpr T const* end() const noexcept { return &elems[N]; }
     constexpr T const* cend() const noexcept { return &elems[N]; }
     constexpr size_t size() const noexcept { return N; }
+    constexpr T & operator[] (size_t i) noexcept { return elems[i]; }
 };
 
-constexpr auto add(int value) noexcept {
+constexpr auto append(int value) noexcept {
     array<int, 1> one = { value };
     return one;
 }
@@ -30,21 +31,58 @@ constexpr void copy(const A * start, const B * end, C * start_) noexcept {
 }
 
 template<size_t N>
-constexpr auto add(const array<int, N>& ar1, int value) noexcept {
-    array<int, N+1> result {0};
-    copy (ar1.cbegin(), ar1.cend(), result.begin());
+constexpr auto duplicate(const array<int, N>& array_) noexcept {
+    array<int, N> result {0};
+    copy (array_.cbegin(), array_.cend(), result.begin());
+    return result;
+}
 
-    auto one = add(value);
+template<size_t N>
+constexpr auto append(const array<int, N>& array_, int value) noexcept {
+    array<int, N+1> result {0};
+    copy (array_.cbegin(), array_.cend(), result.begin());
+
+    auto one = append(value);
     copy(one.cbegin(), one.cend(), result.end()-1);
 
     return result;
 }
 
-int main()
-{
-    constexpr auto result = add(5);
-    constexpr auto result_ = add(result, 10);
-    for (auto& x : result_) std::cout << x << " ";
+template<typename T>
+constexpr void swap(T *a, T *b) noexcept {
+    const T & c = *a;
+    *a = *b;
+    *b = c;
+}
+
+template<size_t N>
+constexpr auto sort(const array<int, N>& array_) noexcept {
+    auto result = duplicate(array_);
+
+    auto start = result.begin();
+    auto end = result.end();
+
+    for (int i = 0; i < N-1; i++) {
+        if (start[i] > start[i+1]) {
+            int s = start[i+1];
+            start[i+1] = start[i];
+            start[i] = s;
+        }
+    }
+
+    return result;
+}
+
+constexpr auto result = append(10);
+constexpr auto result_ = append(result, 5);
+constexpr auto sorted1 = sort(result_);
+
+constexpr auto sorted2 = sort(append(append(10), 5));
+
+int main() {
+    for (auto& x : sorted1 ) std::cout << x << " ";
+    std::cout << std::endl;
+    for (auto& x : sorted2 ) std::cout << x << " ";
     std::cout << std::endl;
     return 0;
 }
