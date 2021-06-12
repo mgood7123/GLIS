b21e5668aac299b9efe58d6399695bbab10b1332
mgood7123 <smallville7123@gmail.com>
2021-06-12 20:39:16 +1000

    Delete debug_BUILD directory

--- debug_BUILD/dependancies/AnyOpt/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smallville7123/AndroidCompositor/app/src/main/jni/GLIS/dependancies/AnyOpt/googletest/googlemock
-# Build directory: /home/smallville7123/AndroidCompositor/app/src/main/jni/GLIS/debug_BUILD/dependancies/AnyOpt/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- debug_BUILD/dependancies/openal-soft/CMakeFiles/ex-common.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../Debug/lib/libex-common.a"
-)
